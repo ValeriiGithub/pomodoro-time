f7663fa89d8b40cbede3961d5fd91882e4c26261
Valerii <valeri.github@gmail.com>
2024-11-06 17:03:10 +0300

    РабÐОбновлена ручка task и методы ÐCRUD

--- handlers/tasks.py
+++ handlers/tasks.py
@@ -33,7 +33,12 @@ async def create_task(task: Task):
     """
     Создает новую задачу с указанным task_id.
     """
-    fixture_tasks.append(task)
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Tasks (name, pomodoro_count, category_id) VALUES (?,?,?)",
+                   (task.name, task.pomodoro_count, task.category_id))
+    connection.commit()
+    connection.close()
     return task
 
 
@@ -45,21 +50,41 @@ async def patch_task(task_id: int, name: str):
     """
     Обнавляет задачу с указанным task_id.
     """
-    for task in fixture_tasks:
-        if task["id"] == task_id:
-            task["name"] = name
-            return task
-
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Tasks SET name=? WHERE id=?", (name, task_id))
+    connection.commit()
+    task = cursor.execute("SELECT * FROM Tasks WHERE id=?",
+                          (task_id,)).fetchall()[0]
+    connection.close()
+    return Task(
+            id=task[0],
+            name=task[1],
+            pomodoro_count=task[2],
+            category_id=task[3],
+            )
 
 @router.delete(
     "/{task_id}",
     status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete_task(task_id: int):
-    for index, task in enumerate(fixture_tasks):
-        if task["id"] == task_id:
-            del fixture_tasks[index]
-            # fixture_tasks.remove(task_id)  # Удаляет первое вхождение значения task_id
-            return {"message": f"task {task_id} deleted"}
+    """
+    Удаляет задачу с указанным task_id.
+    :param task_id:
+    :return:
+    """
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Tasks WHERE id=?", (task_id,))
+    connection.commit()
+    connection.close()
+    return {"message": f"task {task_id} deleted successfully"}
+
+    # for index, task in enumerate(fixture_tasks):
+    #     if task["id"] == task_id:
+    #         del fixture_tasks[index]
+    #         # fixture_tasks.remove(task_id)  # Удаляет первое вхождение значения task_id
+    #         return {"message": f"task {task_id} deleted"}
 
     return {"message": f"task {task_id} not found"}

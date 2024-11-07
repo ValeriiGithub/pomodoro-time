9f9b660e406bc5922f070f69134d2a37f43598a0
Valerii <valeri.github@gmail.com>
2024-11-07 17:13:58 +0300

    Add explain about '////'

--- database/database.py
+++ database/database.py
@@ -2,7 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # engine = create_engine('sqlite:///' + os.path.join(settings.project_root, settings.sqlite_db_name))
-engine = create_engine('sqlite:///pomodoro.sqlite')
+engine = create_engine('sqlite:////mnt/c/db/pomodoro.sqlite')
+
+"""
+
+sqlite:///path/to/database.db
+
+Здесь три слэша (///) означают, что файл находится в файловой системе, а не в сетевом ресурсе.
+
+Первые два слэша (//) указывают на то, что это файл, а не сетевой ресурс. Третий и четвертый слэши (////) необходимы, 
+чтобы указать, что файл находится на диске Windows, доступном через каталог /mnt/ в WSL.
+
+Таким образом, четыре слэша в начале пути позволяют корректно указать путь к файлу базы данных, расположенной 
+на диске Windows, из среды WSL.
+"""
 
 Session = sessionmaker(bind=engine)
 

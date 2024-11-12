7fa0dfbd602bf6f5e3a066ef4309f1d937e253e4
xeon <valeri.github@gmail.com>
2024-11-12 14:15:50 +0300

    Add dependecy file

---
+++ dependecy.py
@@ -0,0 +1,19 @@
+# Dependency
+from database import get_db_session
+from repository import TaskRepository
+
+
+def get_task_repository() -> TaskRepository:
+    """
+    Назначение функции get_task_repository():
+
+    Эта функция предоставляет единый точку доступа для получения экземпляра TaskRepository.
+    Она скрывает детали создания TaskRepository, такие, как получение объекта Session из базы данных.
+    Это позволяет легко заменить реализацию TaskRepository (например, если вы решите использовать другую базу данных или другой ORM) без необходимости изменять код, который использует TaskRepository.
+    Такой подход называется "внедрение зависимостей" (Dependency Injection) и помогает сделать код более модульным, тестируемым и гибким.
+
+    Таким образом, функция get_task_repository() играет важную роль в обеспечении правильного создания и использования TaskRepository в  приложении.
+    :return: TaskRepository
+    """
+    db_session = get_db_session()
+    return TaskRepository(db_session)

f3f87f85e884e39e2ebd2fd2d02af111ba74d7dd
xeon <valeri.github@gmail.com>
2024-11-15 11:54:43 +0300

    FIX

--- repository/cache_task.py
+++ repository/cache_task.py
@@ -7,9 +7,9 @@ class TaskCache:
     def __init__(self, redis: Redis):
         self.redis = redis
 
-        def get_tasks(self):
-            pass
+    def get_tasks(self):
+        pass
 
-        def set_tasks(self, tasks: TaskSchema):
-            tasks_json = [task.json() for task in tasks]
-            self.redis.set("tasks", *tasks_json)
+    def set_tasks(self, tasks: TaskSchema):
+        tasks_json = [task.json() for task in tasks]
+        self.redis.set("tasks", *tasks_json)

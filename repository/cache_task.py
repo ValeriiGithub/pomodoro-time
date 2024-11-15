3a407943ce628e1b73a48fe319c64acd41968fed
xeon <valeri.github@gmail.com>
2024-11-15 11:41:09 +0300

    Create repository cache task

---
+++ repository/cache_task.py
@@ -0,0 +1,15 @@
+from redis import Redis
+
+from schema.task import TaskSchema
+
+
+class CacheTask:
+    def __init__(self, redis: Redis):
+        self.redis = redis
+
+        def get_tasks(self):
+            pass
+
+        def set_tasks(self, tasks: TaskSchema):
+            tasks_json = [task.json() for task in tasks]
+            self.redis.set("tasks", *tasks_json)

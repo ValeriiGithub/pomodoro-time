8d4c9ddf6f13a7daecf0394c851560e58b6d1c2d
xeon <valeri.github@gmail.com>
2024-11-15 12:18:26 +0300

    Create queries

---
+++ queries.sql
@@ -0,0 +1,15 @@
+INSERT INTO categories (name, type)
+VALUES
+    ('Category 1', 1),
+    ('Category 2', 2),
+    ('Category 3', 3),
+    ('Category 4', 4),
+    ('Category 5', 5);
+
+INSERT INTO tasks (name, pomodoro_count, category_id)
+VALUES
+    ('Task 1', 2, 1),
+    ('Task 2', 3, 2),
+    ('Task 3', 1, 1),
+    ('Task 4', 4, 3),
+    ('Task 5', 2, 2);

814fc300f20052d20f607c9bc33f313828118d94
xeon <valeri.github@gmail.com>
2024-11-19 18:02:02 +0300

    feature: create auth service

--- service/auth.py
+++ service/auth.py
@@ -1,16 +1,20 @@
-import string
 from dataclasses import dataclass
-from random import choice
+import datetime as dt
+from datetime import timedelta
+
+from jose import jwt
 
 from models import UserProfile
 from exception import UserNotFoundException, UserNotCorrectPasswordException
 from repository import UserRepository
 from schema import UserLoginSchema
+from settings import Settings
 
 
 @dataclass
 class AuthService:
     user_repository: UserRepository
+    settings: Settings
 
     def login(self, username: str, password: str) -> UserLoginSchema:
         user = self.user_repository.get_user_by_username(username)
@@ -25,6 +29,15 @@ class AuthService:
         if user.password != password:
             raise UserNotCorrectPasswordException
 
-    @staticmethod
-    def generate_access_token(user_id: int) -> str:
-        return ''.join(choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+    def generate_access_token(self, user_id: int) -> str:
+        # Возможный fix baga
+        # expire_date = dt.datetime.now(dt.timezone.utc) + timedelta(days=7)  # Expire in 7 days
+        # expire_date_unix = expire_date.timestamp()
+        expire_date_unix = (dt.datetime.utcnow() + timedelta(days=7)).timestamp()  # Expire in 1 day
+        token = jwt.encode(
+            {'user_id': user_id, 'expire': expire_date_unix},
+            self.settings.JWT_SECRET_KEY,
+            algorithm=self.settings.JWT_ENCODE_ALGORITHM,
+        )
+        return token

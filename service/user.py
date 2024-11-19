105a4f851dd97d9894747250f1b71f5eb8f69c1c
xeon <valeri.github@gmail.com>
2024-11-19 17:57:26 +0300

    FIX: fix atribute

--- service/user.py
+++ service/user.py
@@ -2,15 +2,15 @@ from dataclasses import dataclass
 
 from schema import UserLoginSchema
 from repository import UserRepository
-from service import AuthService
+from service.auth import AuthService
 
 
 @dataclass
 class UserService:
     user_repository: UserRepository
-    user_service: AuthService
+    auth_service: AuthService
 
     def create_user(self, username: str, password: str) -> UserLoginSchema:
         user = self.user_repository.create_user(username=username, password=password)
-        access_token = self.user_service.generate_access_token(user_id=user.id)
+        access_token = self.auth_service.generate_access_token(user_id=user.id)
         return UserLoginSchema(user_id=user.id, access_token=access_token)
